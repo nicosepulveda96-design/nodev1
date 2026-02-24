1a759b49cbbd484469a6d3698a41af19a5c2745f
Nicolás Sepúlveda <nicosepulveda96@gmail.com>
2026-02-24 17:10:34 -0500

    clase 24/02/2026

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -14,7 +14,53 @@ const listar = async (req, res) => {
     const usuarios = await usuarioService.listarUsuarios();
     res.json(usuarios);
 };
+
+//consultar un usuario por id
+const obtenerUno = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const usuario = await usuarioService.obtenerUsuarioPorId(id);
+        if(!usuario){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+        res.json(usuario);
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+};
+
+//actualizar
+const actualizar = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const usuarioActualizado =  await usuarioService.actualizarUsuario(id, req.body);
+        if(!usuarioActualizado){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+       res.json(usuarioActualizado);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+};
+
+// eliminar
+const eliminar = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const eliminar = await usuarioService.eliminarUsuario(id);
+        if(!eliminar){
+            return res.status(404).json({error: 'Usuario no encontrado'});
+        }
+        res.json({message: 'Usuario eliminado correctamente'});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+};
+
 module.exports = {
     crear,
-    listar
-};
+    listar,
+    obtenerUno,
+    actualizar,
+    eliminar
+};  
--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -7,8 +7,36 @@ const crearUsuario = async (data) => {
 const listarUsuarios = async ()=>{
     return await Usuario.findAll();
 };
+
+// obtener un usuario por id
+const obtenerUsuarioPorId = async (id)=> {
+    return await Usuario.findByPk(id);
+};
+// actualizar
+const actualizarUsuario = async (id, data) => {
+    const usuario = await Usuario.findByPk(id);
+    if(!usuario){
+        return null;
+    }
+    await usuario.update(data);
+    return usuario;
+
+};
+// eliminar
+const eliminarUsuario = async (id) => {
+    const usuario = await Usuario.findByPk(id);
+    if(!usuario){
+        return null;
+    }
+    await usuario.destroy();
+    return true;
+};
+
 module.exports ={
     crearUsuario,
-    listarUsuarios
+    listarUsuarios,
+    obtenerUsuarioPorId,
+    actualizarUsuario,
+    eliminarUsuario
 };
 
